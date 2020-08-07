f8fda565cf0f499eeb14b8398651d26076887f1f
= <=>
2020-08-07 14:54:20 +0800

    add vim plugin

--- install.sh
+++ install.sh
@@ -13,14 +13,33 @@ sudo config --global user.email "p870613@yahoo.com.tw"
 
 # vim install and setting
 sudo apt install vim 
-cp -r ./vim ~/
+
+#zsh install 
+sudo apt install zsh
+chsh -s /usr/bin/zsh
+apt install wget git
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+source ~/.zshrc
+
+#vindlle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./vimrc ~/
+
+# install youcompleteme needed package
+sudo apt-get install build-essential cmake python-dev python3-dev
 
 # install youcomplete me
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/plugged/
-git submodule update --init --recursive
 cd ~/.vim/plugged/YouCompleteMe
-./install.py --clang-completer
-./install.py --rust-completer
+git submodule update --init --recursive
+python3 install.py --clang-completer
+python3 ./install.py --rust-completer
 
-
+#install racer
+cd ~/
+rustup toolchain add nightly
+cargo +nightly install racer
+rustup component add rust-src
+echo RUST_SRC_PATH=$(rustc --print sysroot)/lib/rustlib/src/rust/src >> ~/.zshrc
+racer complete std::io::B
